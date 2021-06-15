fc0edd5ae786716442c0f4f8b36bf49a705b66ae
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-06-15 18:55:21 -0300

    Add github workflows

--- aqua/extensions/commands/start.py
+++ aqua/extensions/commands/start.py
@@ -1,2 +1,6 @@
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! This is the start command")
+from telegram import Update
+from telegram.ext.callbackcontext import CallbackContext
+
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! This is the start command")  # type: ignore
