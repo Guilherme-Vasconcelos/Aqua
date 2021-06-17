3f28fbe0d97f24e94ae4a1330629a377a13b9eec
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-06-16 22:45:43 -0300

    Improve logging for start command

--- aqua/extensions/commands/start.py
+++ aqua/extensions/commands/start.py
@@ -1,3 +1,5 @@
+import logging
+
 from telegram import Update
 from telegram.ext.callbackcontext import CallbackContext
 
@@ -6,4 +8,7 @@ from aqua.checks import authorize
 
 @authorize
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome!")
+    message_to_send = 'Welcome!'
+    chat_to_send = update.effective_chat.id
+    logging.info(f'Sending message \'{message_to_send}\' to authorized user \'{chat_to_send}\'')
+    context.bot.send_message(chat_id=chat_to_send, text=message_to_send)
